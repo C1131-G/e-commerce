4582a4d0ab3ecf352441e4f4752dfafdb372866f
siva sankar <medibionasiva7@gmail.com>
2026-07-30 17:50:46 +0530

    refactor: move auth and db into workspace packages

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -1,8 +1,8 @@
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { db } from "@workspace/db";
 import { betterAuth } from "better-auth";
 
-import * as schema from "../auth-schema";
-import { db } from "./db";
+import * as schema from "./auth-schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
